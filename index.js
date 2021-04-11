4c2cd47c2ed4ff8c536911395a0b491417e709df
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-04-11 14:10:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // Write your solution here!
-pets.push('Odie');
+// First, we're going to create four arrays of pets. Define 4 `const` called:
+// `append`, `prepend`, `removeLast` and `removeFirst`, and set all to an initial
+// value of `["Milo", "Otis", "Garfield"]`. These are the `array`s that we'll work
+// with for each exercise.
+const pets = ["Milo", "Otis", "Garfield"];
+pets.push("Odie");
